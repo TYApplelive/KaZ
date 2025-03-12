be68f39d4b099a214b8451610835c9c9044b1cea
_Asialing_ <96574207+TYApplelive@users.noreply.github.com>
2025-03-12 20:14:57 +0800

    VUE3初始化

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // 允许外部访问（例如通过宿主机访问容器内的服务）
+    port: 8081,
+    hmr: true, // 启用热模块替换
+    watch: {
+      usePolling: true, // 启用轮询机制监听文件变化
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
